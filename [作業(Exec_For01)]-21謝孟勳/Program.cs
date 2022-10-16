02cebac446b6f9e6bd3eebf5b7f39c2e3f1e0d53
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:15:14 +0800

    加入專案檔案。

---
+++ [作業(Exec_For01)]-21謝孟勳/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_For01___21謝孟勳
+{
+    //倒星形三角形
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = string.Empty;
+            
+            for (int i = 10; i > 0; i--)
+            {
+                string row = new string('*', i);
+                Console.WriteLine(row);
+            }
+            
+            
+        }
+    }
+}
